2b9f5a0d5ad0a513732de94332dde85b2a4ed5c0
Steve Sewell <steve@builder.io>
2022-08-09 19:02:13 -0700

    updates

--- frameworks/qwik/src/routes/index.tsx
+++ frameworks/qwik/src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { component$, Host } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
+import { HelloWorld } from '@builder.io/mitosis-app-hello-world/qwik'
 
 export default component$(() => {
   return (
     <Host>
-      <h1>Hello Qwik</h1>
+      <HelloWorld name="Qwik" />
     </Host>
   );
 });
--- frameworks/qwik/src/routes/todo.tsx
+++ frameworks/qwik/src/routes/todo.tsx
@@ -1,17 +1,15 @@
 import { component$, Host } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
+import { ToDoApp } from '@builder.io/mitosis-app-todo/qwik';
 
 export default component$(() => {
   return (
     <Host>
-      <h1>About Us</h1>
-      <p>
-        <a href="/">Home</a>
-      </p>
+      <ToDoApp />
     </Host>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'About Us',
+  title: 'Welcome to Qwik City',
 };
