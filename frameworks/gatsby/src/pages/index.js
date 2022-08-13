f0c8c7ba4db38de32f6c56ca3b58c617766f64bb
Chris LaRocque <larocque.christopher@gmail.com>
2022-08-13 11:13:14 -0400

    Update index.js
    missed framework name

--- frameworks/gatsby/src/pages/index.js
+++ frameworks/gatsby/src/pages/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { HelloWorld } from '@builder.io/components/react';
 
 export default function Home() {
-  return <HelloWorld name="Next.js" />;
+  return <HelloWorld name="Gatsby" />;
 }
