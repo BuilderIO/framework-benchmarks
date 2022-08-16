03a5c46e4800f1d9e801969c03cb6bf8d70de8a2
Sami Jaber <jabersami@gmail.com>
2022-08-16 16:47:44 -0400

    add svelte preprocess

--- frameworks/svelte/svelte.config.js
+++ frameworks/svelte/svelte.config.js
@@ -1,10 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
-	}
+	},
+	preprocess: preprocess()
 };
 
 export default config;
