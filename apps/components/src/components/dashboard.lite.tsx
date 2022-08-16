c144896a7731b8ca29893b0e13ea9a7adb6bdb33
Steve Sewell <steve@builder.io>
2022-08-16 08:45:43 -0700

    dashboard WIP

--- apps/components/src/components/dashboard.lite.tsx
+++ apps/components/src/components/dashboard.lite.tsx
@@ -1,6 +1,6 @@
 import { useStore } from '@builder.io/mitosis';
-import helloWorldData from '../../reports/qwik.json';
-import todoData from '../../reports/todo/qwik.json';
+// import helloWorldData from '../../reports/angular.json';
+// import todoData from '../../reports/todo/angular.json';
 import Chart from './dashboard/chart.lite';
 import Table from './dashboard/dash-table.lite';
 import FrameworkPicker from './dashboard/framework-picker.lite';
@@ -8,9 +8,25 @@ import { Framework } from './dashboard/frameworks';
 
 export default function Dashboard() {
   const state = useStore({
+    loading: false,
     framework: 'angular' as Framework,
+    currentData: null as LH.Result[] | null,
     getData() {
-      return [helloWorldData, todoData];
+      // return state.currentData || [helloWorldData, todoData];
+    },
+    async changeFamework(framework: Framework) {
+      state.framework = framework;
+      state.loading = true;
+      // if (framework === 'angular') {
+      //   state.currentData = [helloWorldData, todoData];
+      // } else if (framework === 'astro') {
+      //   state.currentData = await Promise.all([
+      //     import('../../reports/astro.json'),
+      //     import('../../reports/todo/astro.json'),
+      //   ]);
+      // }
+      state.loading = false;
+      state.currentData = null;
     },
   });
 
@@ -18,7 +34,7 @@ export default function Dashboard() {
     <>
       <FrameworkPicker
         value={state.framework}
-        onChange={(event) => (state.framework = event)}
+        onChange={(event) => state.changeFamework(event)}
       />
       <Table data={state.getData()} />
       <Chart data={state.getData()} />
