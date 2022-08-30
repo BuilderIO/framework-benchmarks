97c2a313ba27c97412928927e0a80d9f7170fe59
Pooya Parsa <pooya@pi0.io>
2022-08-30 20:31:14 +0200

    update to use nitro compression

--- frameworks/nuxt3/nuxt.config.ts
+++ frameworks/nuxt3/nuxt.config.ts
@@ -3,6 +3,7 @@ import { defineNuxtConfig } from 'nuxt';
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   nitro: {
+    compressPublicAssets: true,
     prerender: {
       routes: [
         '/',
